949c21345564f7b93d4c9557517417d581618128
blicake <blicake.mail@gmail.com>
2023-12-21 04:25:05 +0500

    added database connection

--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,9 @@
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI
+from pydantic import BaseModel
+import sqlite3
 import os
-
-usernames = []
 
 app = FastAPI()
 
@@ -11,13 +11,35 @@ static_path = os.path.join(os.path.dirname(__file__), "static")
 
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
+conn = sqlite3.connect('questions.db')
+cursor = conn.cursor()
+
+# Create users and questions tables
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, question TEXT)")
+
 @app.get("/")
 async def root() -> dict:
     return FileResponse("web-app/page.html")
 
-@app.post("/login")
-async def add_username(username: str) -> dict:
-    if username and username not in usernames:
-        usernames.append(username)
-        return {"message": "Username added successfully"}
-    return {"message": "Username already exists or invalid"}
+@app.post("/add-username/{username}")
+async def add_username(username: str):
+    cursor.execute("INSERT INTO users (username) VALUES (?)", (username,))
+    conn.commit()
+    return {"message": f"Username {username} added successfully"}
+
+class QuestionData(BaseModel):
+    username: str
+    question: str
+
+@app.post("/add-question")
+async def add_question(data: QuestionData):
+    cursor.execute("INSERT INTO questions (username, question) VALUES (?, ?)", (data.username, data.question))
+    conn.commit()
+    return {"message": f"Question {data.question} from {data.username} added successfully"}
+
+@app.get("/get-questions")
+async def get_questions():
+    result = cursor.execute("SELECT username, question FROM questions").fetchall()
+    questions = [{"username": row[0], "question": row[1]} for row in result]
+    return {"questions": questions}
--- web-app/static/script.js
+++ web-app/static/script.js
@@ -1,43 +1,80 @@
-let usernames = [];
-
-let currentUsername = "";
+currentUsername = ''
 
 function addUsername() {
   const usernameInput = document.getElementById('usernameInput');
   const username = usernameInput.value;
 
   if (username) {
-    currentUsername = username;
-    document.getElementById('displayedUsername').innerText = `Logged in as: ${username}`; // Display the username
-    usernameInput.value = '';
+    fetch(`/add-username/${username}`, {
+      method: 'POST'
+    })
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('displayedUsername').innerText = `Logged in as: ${username}`;
+        usernameInput.value = '';
+        currentUsername = `${username}`;
+      });
   }
 }
 
-
-function addQuestion() {
+async function addQuestion() {
   const questionInput = document.getElementById('questionInput');
   const questionText = questionInput.value;
-  
-  if (currentUsername && questionText) {
+  console.debug(usernameInput);
+  const url = '/add-question';
+  const data = {
+    username: currentUsername,
+    question: questionText
+  };
+  console.debug(data);
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (response.ok) {
     const questionList = document.getElementById('questionList');
     const listItem = document.createElement('li');
-    listItem.innerText = `${currentUsername}: ${questionText}`; // Connect the question to the current username
+    listItem.innerText = `${currentUsername}: ${questionText}`;
     listItem.addEventListener('click', viewAnswers);
     questionList.appendChild(listItem);
     questionInput.value = '';
+  } else {
+    console.error('Failed to add question:', response.statusText);
   }
+}
+
+function getQuestions() {
+  fetch('/get-questions')
+    .then(response => response.json())
+    .then(data => {
+      if (data && data.questions) {
+        const questions = data.questions;
+        const questionList = document.getElementById('questionList');
+        questionList.innerHTML = '';
+        questions.forEach(({ username, question }) => {
+          const listItem = document.createElement('li');
+          listItem.innerText = `${username}: ${question}`;
+          listItem.addEventListener('click', viewAnswers);
+          questionList.appendChild(listItem);
+        });
+      }
+    });
 }
 
 function viewAnswers(event) {
   const question = event.target.innerText;
-  const answers = prompt(`Answers for "${question}":`);
+  const username = question.split(':')[0].trim();
+  const questionText = question.split(':')[1].trim();
+  const answers = prompt(`Answers for "${questionText}" by ${username}:`);
   if (answers) {
-    alert(answers);
+    // Handle answers, e.g., send to backend
   }
 }
 
-document.getElementById('questionList').addEventListener('click', function(event) {
-  if (event.target.tagName === 'LI') {
-    viewAnswers(event);
-  }
-});
+document.getElementById('questionList').addEventListener('click', viewAnswers);
+
+getQuestions();
